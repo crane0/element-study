6d5a32144712e3532993a92b83e5d3298b1ceb48
crane0 <bhad5683126@163.com>
2023-04-06 23:56:51 +0800

    feat: tooltip 组件注释

--- packages/tooltip/src/main.js
+++ packages/tooltip/src/main.js
@@ -1,75 +1,84 @@
-import Popper from 'element-ui/src/utils/vue-popper';
-import debounce from 'throttle-debounce/debounce';
-import { addClass, removeClass, on, off } from 'element-ui/src/utils/dom';
-import { generateId } from 'element-ui/src/utils/util';
-import Vue from 'vue';
+import Popper from "element-ui/src/utils/vue-popper";
+import debounce from "throttle-debounce/debounce";
+import { addClass, removeClass, on, off } from "element-ui/src/utils/dom";
+import { generateId } from "element-ui/src/utils/util";
+import Vue from "vue";
 
 export default {
-  name: 'ElTooltip',
+  name: "ElTooltip",
 
   mixins: [Popper],
 
   props: {
     openDelay: {
       type: Number,
-      default: 0
+      default: 0,
     },
     disabled: Boolean,
-    manual: Boolean,
+    manual: Boolean, // 手动控制模式，设置为 true 后，mouseenter 和 mouseleave 事件将不会生效
     effect: {
+      // 主题
       type: String,
-      default: 'dark'
+      default: "dark",
     },
     arrowOffset: {
+      // 后来的版本取消该配置了，
       type: Number,
-      default: 0
+      default: 0,
     },
     popperClass: String,
     content: String,
     visibleArrow: {
-      default: true
+      // 是否显示 Tooltip 箭头
+      default: true,
     },
     transition: {
       type: String,
-      default: 'el-fade-in-linear'
+      default: "el-fade-in-linear",
     },
     popperOptions: {
       default() {
         return {
-          boundariesPadding: 10,
-          gpuAcceleration: false
+          boundariesPadding: 10, // 用于定义边界和 popper 之间的最小距离的像素数量
+          gpuAcceleration: false,
         };
-      }
+      },
     },
     enterable: {
+      // 鼠标是否可进入到 tooltip 中（鼠标进入后是否关闭 tooltip）
       type: Boolean,
-      default: true
+      default: true,
     },
     hideAfter: {
+      // Tooltip 出现后自动隐藏延时，单位毫秒，为 0 则不会自动隐藏
       type: Number,
-      default: 0
+      default: 0,
     },
     tabindex: {
       type: Number,
-      default: 0
-    }
+      default: 0,
+    },
   },
 
   data() {
     return {
       tooltipId: `el-tooltip-${generateId()}`,
       timeoutPending: null,
-      focusing: false
+      focusing: false, // class 类名
     };
   },
   beforeCreate() {
     if (this.$isServer) return;
 
+    // 参考 https://v2.cn.vuejs.org/v2/api/#vm-mount
+    // 不设置 el 属性，并且 $mount() 也没有指定 DOM 元素，则在文档之外渲染并且随后挂载，
+    // 比如 document.getElementById('app').appendChild(this.popperVM.$el)
+    // 最终会把这个创建的这个 popper 在 vue-popper 中添加到 body 上。
     this.popperVM = new Vue({
-      data: { node: '' },
+      data: { node: "" },
       render(h) {
         return this.node;
-      }
+      },
     }).$mount();
 
     this.debounceClose = debounce(200, () => this.handleClosePopper());
@@ -78,46 +87,58 @@ export default {
   render(h) {
     if (this.popperVM) {
       this.popperVM.node = (
-        <transition
-          name={ this.transition }
-          onAfterLeave={ this.doDestroy }>
+        <transition name={this.transition} onAfterLeave={this.doDestroy}>
           <div
-            onMouseleave={ () => { this.setExpectedState(false); this.debounceClose(); } }
-            onMouseenter= { () => { this.setExpectedState(true); } }
+            onMouseleave={() => {
+              this.setExpectedState(false);
+              this.debounceClose();
+            }}
+            onMouseenter={() => {
+              this.setExpectedState(true);
+            }}
             ref="popper"
             role="tooltip"
             id={this.tooltipId}
-            aria-hidden={ (this.disabled || !this.showPopper) ? 'true' : 'false' }
+            aria-hidden={this.disabled || !this.showPopper ? "true" : "false"}
             v-show={!this.disabled && this.showPopper}
-            class={
-              ['el-tooltip__popper', 'is-' + this.effect, this.popperClass]
-            }>
-            { this.$slots.content || this.content }
+            class={[
+              "el-tooltip__popper",
+              "is-" + this.effect,
+              this.popperClass,
+            ]}
+          >
+            {this.$slots.content || this.content}
           </div>
-        </transition>);
+        </transition>
+      );
     }
 
     const firstElement = this.getFirstElement();
     if (!firstElement) return null;
 
-    const data = firstElement.data = firstElement.data || {};
+    const data = (firstElement.data = firstElement.data || {});
+    // 元素的 class（string）
     data.staticClass = this.addTooltipClass(data.staticClass);
 
     return firstElement;
   },
 
   mounted() {
+    // 注意，this.$el 是指 render 返回的 VNode，也就是 <el-tooltip><button></button></el-tooltip> 中的button
+    // 而且必须是元素节点。否则不展示。
     this.referenceElm = this.$el;
     if (this.$el.nodeType === 1) {
-      this.$el.setAttribute('aria-describedby', this.tooltipId);
-      this.$el.setAttribute('tabindex', this.tabindex);
-      on(this.referenceElm, 'mouseenter', this.show);
-      on(this.referenceElm, 'mouseleave', this.hide);
-      on(this.referenceElm, 'focus', () => {
+      this.$el.setAttribute("aria-describedby", this.tooltipId);
+      this.$el.setAttribute("tabindex", this.tabindex);
+      on(this.referenceElm, "mouseenter", this.show);
+      on(this.referenceElm, "mouseleave", this.hide);
+      on(this.referenceElm, "focus", () => {
+        // this.$slots.default 不就是 firstElement，也就是 this.referenceElm，那下面这个语句应该不会执行吧。
         if (!this.$slots.default || !this.$slots.default.length) {
           this.handleFocus();
           return;
         }
+        // 当 <el-tooltip>...</el-tooltip>包裹的是组件时，就变成判断该组件实例是否聚焦了。
         const instance = this.$slots.default[0].componentInstance;
         if (instance && instance.focus) {
           instance.focus();
@@ -125,10 +146,11 @@ export default {
           this.handleFocus();
         }
       });
-      on(this.referenceElm, 'blur', this.handleBlur);
-      on(this.referenceElm, 'click', this.removeFocusing);
+      on(this.referenceElm, "blur", this.handleBlur);
+      on(this.referenceElm, "click", this.removeFocusing);
     }
     // fix issue https://github.com/ElemeFE/element/issues/14424
+    // issue 的表现: 如果直接设置 value = true，但 tooltip 一开始不显示的问题。
     if (this.value && this.popperVM) {
       this.popperVM.$nextTick(() => {
         if (this.value) {
@@ -140,11 +162,11 @@ export default {
   watch: {
     focusing(val) {
       if (val) {
-        addClass(this.referenceElm, 'focusing');
+        addClass(this.referenceElm, "focusing");
       } else {
-        removeClass(this.referenceElm, 'focusing');
+        removeClass(this.referenceElm, "focusing");
       }
-    }
+    },
   },
   methods: {
     show() {
@@ -156,6 +178,7 @@ export default {
       this.setExpectedState(false);
       this.debounceClose();
     },
+    // 聚焦展示，失焦关闭
     handleFocus() {
       this.focusing = true;
       this.show();
@@ -168,11 +191,13 @@ export default {
       this.focusing = false;
     },
 
+    // 可以看到，el-tooltip 这个类名是加到 referenceElm 上的。
+    // popper 上的类名是 el-tooltip__popper
     addTooltipClass(prev) {
       if (!prev) {
-        return 'el-tooltip';
+        return "el-tooltip";
       } else {
-        return 'el-tooltip ' + prev.replace('el-tooltip', '');
+        return "el-tooltip " + prev.replace("el-tooltip", "");
       }
     },
 
@@ -191,7 +216,7 @@ export default {
     },
 
     handleClosePopper() {
-      if (this.enterable && this.expectedState || this.manual) return;
+      if ((this.enterable && this.expectedState) || this.manual) return;
       clearTimeout(this.timeout);
 
       if (this.timeoutPending) {
@@ -219,10 +244,10 @@ export default {
         if (slots[index] && slots[index].tag) {
           element = slots[index];
           break;
-        };
+        }
       }
       return element;
-    }
+    },
   },
 
   beforeDestroy() {
@@ -232,11 +257,11 @@ export default {
   destroyed() {
     const reference = this.referenceElm;
     if (reference.nodeType === 1) {
-      off(reference, 'mouseenter', this.show);
-      off(reference, 'mouseleave', this.hide);
-      off(reference, 'focus', this.handleFocus);
-      off(reference, 'blur', this.handleBlur);
-      off(reference, 'click', this.removeFocusing);
+      off(reference, "mouseenter", this.show);
+      off(reference, "mouseleave", this.hide);
+      off(reference, "focus", this.handleFocus);
+      off(reference, "blur", this.handleBlur);
+      off(reference, "click", this.removeFocusing);
     }
-  }
+  },
 };
--- src/utils/vue-popper.js
+++ src/utils/vue-popper.js
@@ -1,10 +1,10 @@
-import Vue from 'vue';
-import {
-  PopupManager
-} from 'element-ui/src/utils/popup';
+import Vue from "vue";
+import { PopupManager } from "element-ui/src/utils/popup";
 
-const PopperJS = Vue.prototype.$isServer ? function() {} : require('./popper');
-const stop = e => e.stopPropagation();
+// popperjs 2.x 中文教程 https://www.jiyik.com/w/popperjs/popper-basic-play
+// 但是引入的这个 Popper 是 1.x 版本的。英文官网 https://popper.js.org/docs/v1/
+const PopperJS = Vue.prototype.$isServer ? function() {} : require("./popper");
+const stop = (e) => e.stopPropagation();
 
 /**
  * @param {HTMLElement} [reference=$refs.reference] - The reference element used to position the popper.
@@ -17,46 +17,51 @@ const stop = e => e.stopPropagation();
 export default {
   props: {
     transformOrigin: {
+      // 元素进行 transform 的原点位置
       type: [Boolean, String],
-      default: true
+      default: true,
     },
     placement: {
+      // 展示位置
       type: String,
-      default: 'bottom'
+      default: "bottom",
     },
     boundariesPadding: {
+      // 应该作为 popperOptions 的属性，但这里没有用到。
+      // 用于定义边界和 popper 之间的最小距离的像素数量
       type: Number,
-      default: 5
+      default: 5,
     },
     reference: {},
     popper: {},
     offset: {
-      default: 0
+      default: 0,
     },
     value: Boolean,
-    visibleArrow: Boolean,
+    visibleArrow: Boolean, // 是否显示 Tooltip 箭头
     arrowOffset: {
       type: Number,
-      default: 35
+      default: 35,
     },
     appendToBody: {
       type: Boolean,
-      default: true
+      default: true,
     },
     popperOptions: {
+      // popper.js 的参数
       type: Object,
       default() {
         return {
-          gpuAcceleration: false
+          gpuAcceleration: false,
         };
-      }
-    }
+      },
+    },
   },
 
   data() {
     return {
       showPopper: false,
-      currentPlacement: ''
+      currentPlacement: "",
     };
   },
 
@@ -65,61 +70,81 @@ export default {
       immediate: true,
       handler(val) {
         this.showPopper = val;
-        this.$emit('input', val);
-      }
+        this.$emit("input", val);
+      },
     },
 
     showPopper(val) {
       if (this.disabled) return;
       val ? this.updatePopper() : this.destroyPopper();
-      this.$emit('input', val);
-    }
+      this.$emit("input", val);
+    },
   },
 
   methods: {
     createPopper() {
       if (this.$isServer) return;
       this.currentPlacement = this.currentPlacement || this.placement;
-      if (!/^(top|bottom|left|right)(-start|-end)?$/g.test(this.currentPlacement)) {
+      if (
+        !/^(top|bottom|left|right)(-start|-end)?$/g.test(this.currentPlacement)
+      ) {
         return;
       }
 
-      const options = this.popperOptions;
-      const popper = this.popperElm = this.popperElm || this.popper || this.$refs.popper;
-      let reference = this.referenceElm = this.referenceElm || this.reference || this.$refs.reference;
+      /* 
+        创建 PopperJS 实例时，其实是调用了 Popper.createPopper 构造函数。
+        createPopper(reference, popper, options);
+        reference 是触发 Popper 的元素，比如可以是一个 button。
+        popper 是存放提示信息的元素。
+        options 是配置项。
 
-      if (!reference &&
-        this.$slots.reference &&
-        this.$slots.reference[0]) {
+        其中 reference 和 popper 可能是同一个元素。
+      */
+      const options = this.popperOptions;
+      // 链式赋值，假设 a b c 都已初始化，则 a = b = c 结果：a 和 b 都为 c 的值。
+      // this.$refs.popper，使用该 mixin 的组件的根元素，一般会设置该 ref
+      const popper = (this.popperElm =
+        this.popperElm || this.popper || this.$refs.popper);
+      // this.referenceElm，使用该 mixin 的组件中会定义，一般为 $el
+      let reference = (this.referenceElm =
+        this.referenceElm || this.reference || this.$refs.reference);
+
+      if (!reference && this.$slots.reference && this.$slots.reference[0]) {
         reference = this.referenceElm = this.$slots.reference[0].elm;
       }
 
       if (!popper || !reference) return;
       if (this.visibleArrow) this.appendArrow(popper);
+      // 这行代码是必须的（因为没有 else），不设置将无法挂载 popper
       if (this.appendToBody) document.body.appendChild(this.popperElm);
       if (this.popperJS && this.popperJS.destroy) {
         this.popperJS.destroy();
       }
 
       options.placement = this.currentPlacement;
-      options.offset = this.offset;
+      options.offset = this.offset; // 出现位置的偏移量
       options.arrowOffset = this.arrowOffset;
       this.popperJS = new PopperJS(reference, popper, options);
-      this.popperJS.onCreate(_ => {
-        this.$emit('created', this);
+      this.popperJS.onCreate((_) => {
+        this.$emit("created", this);
         this.resetTransformOrigin();
         this.$nextTick(this.updatePopper);
       });
-      if (typeof options.onUpdate === 'function') {
+      if (typeof options.onUpdate === "function") {
+        // onUpdate的回调函数，也就是这里的 options.onUpdate 不会在 popper 的初始化/创建时调用，而只会在后续更新时调用。
         this.popperJS.onUpdate(options.onUpdate);
       }
+      // 每个 Popper 的 z-index 统一管理
       this.popperJS._popper.style.zIndex = PopupManager.nextZIndex();
-      this.popperElm.addEventListener('click', stop);
+      // 点击出现的提示信息元素，阻止冒泡
+      this.popperElm.addEventListener("click", stop);
     },
 
+    // watch showPopper 来调用 updatePopper，再判断是 create 还是 update
     updatePopper() {
       const popperJS = this.popperJS;
       if (popperJS) {
+        // 更新 popper 的位置，计算新的偏移量并应用新的样式。并且如果传入了 onUpdate，会在最后执行。
         popperJS.update();
         if (popperJS._popper) {
           popperJS._popper.style.zIndex = PopupManager.nextZIndex();
@@ -145,16 +170,22 @@ export default {
     resetTransformOrigin() {
       if (!this.transformOrigin) return;
       let placementMap = {
-        top: 'bottom',
-        bottom: 'top',
-        left: 'right',
-        right: 'left'
+        top: "bottom",
+        bottom: "top",
+        left: "right",
+        right: "left",
       };
-      let placement = this.popperJS._popper.getAttribute('x-placement').split('-')[0];
+      // x-placement 就是 placement，取值包括 top/top-start/top-end/bottom...
+      let placement = this.popperJS._popper
+        .getAttribute("x-placement")
+        .split("-")[0];
       let origin = placementMap[placement];
-      this.popperJS._popper.style.transformOrigin = typeof this.transformOrigin === 'string'
-        ? this.transformOrigin
-        : ['top', 'bottom'].indexOf(placement) > -1 ? `center ${ origin }` : `${ origin } center`;
+      this.popperJS._popper.style.transformOrigin =
+        typeof this.transformOrigin === "string"
+          ? this.transformOrigin
+          : ["top", "bottom"].indexOf(placement) > -1
+          ? `center ${origin}`
+          : `${origin} center`;
     },
 
     appendArrow(element) {
@@ -165,6 +196,7 @@ export default {
 
       this.appended = true;
 
+      // 可直接用 for...of 遍历
       for (let item in element.attributes) {
         if (/^_v-/.test(element.attributes[item].name)) {
           hash = element.attributes[item].name;
@@ -172,27 +204,30 @@ export default {
         }
       }
 
-      const arrow = document.createElement('div');
+      const arrow = document.createElement("div");
 
       if (hash) {
-        arrow.setAttribute(hash, '');
+        arrow.setAttribute(hash, "");
       }
-      arrow.setAttribute('x-arrow', '');
-      arrow.className = 'popper__arrow';
+      // 在 examples\components\theme\components-preview.vue 中有例子，
+      // 对应的在线链接 https://element.eleme.io/#/zh-CN/theme/preview 中，可以检查元素看看效果。
+      arrow.setAttribute("x-arrow", "");
+      arrow.className = "popper__arrow";
       element.appendChild(arrow);
-    }
+    },
   },
 
   beforeDestroy() {
     this.doDestroy(true);
     if (this.popperElm && this.popperElm.parentNode === document.body) {
-      this.popperElm.removeEventListener('click', stop);
+      this.popperElm.removeEventListener("click", stop);
       document.body.removeChild(this.popperElm);
     }
   },
 
-  // call destroy in keep-alive mode
+  // vue的生命周期，被 keep-alive 缓存的组件失活时调用。
+  // https://v2.cn.vuejs.org/v2/api/index.html#deactivated
   deactivated() {
     this.$options.beforeDestroy[0].call(this);
-  }
+  },
 };
